9d53e60aba4f81e9ed9a74b07f35a4f3e474c23a
Paulo H. Averbeck <phavr@hotmail.com>
2023-06-19 14:40:17 -0300

    aprendendo aula 2

--- app/src/main/java/com/example/carteiramobile/MainActivity.java
+++ app/src/main/java/com/example/carteiramobile/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.carteiramobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);     // Arquivo de layout -> mostra o conteúdo da activity
+
+        View v = findViewById(R.id.buttonSomar);
+        v.setOnClickListener(new View.OnClickListener() { // Criando objeto de classe anônima para evento de click
+            @Override
+            public void onClick(View view) {
+                Log.i("botao", "O botão foi clicado");
+            }
+        });
+
     }
 }
