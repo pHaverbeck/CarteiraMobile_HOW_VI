da96c23a472b2a50e04a1769f96949001390fb6b
Paulo H. Averbeck <phavr@hotmail.com>
2023-06-20 11:33:59 -0300

    aula5

--- app/src/main/java/com/example/carteiramobile/ActivityResultado.java
+++ app/src/main/java/com/example/carteiramobile/ActivityResultado.java
@@ -3,15 +3,27 @@ package com.example.carteiramobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class ActivityResultado extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+public class ActivityResultado extends AppCompatActivity {
+    private static ArrayList<Integer> historico = new ArrayList<Integer>();                         // Criação do espaço de lista de histórico de valores
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
         int soma = this.getIntent().getIntExtra("calculo", -1);
+        historico.add(soma);                                                                        // Valores de soma são guardados aqui no histórico
         ((TextView)findViewById(R.id.textViewResultadoSoma)).setText(String.valueOf(soma));
+        LinearLayout ll = findViewById(R.id.llCalculo);                                             // Referencia o linear layout
+        for(Integer valor: historico){                                                              // Pega o valor do histórico e coloca em um novo textview
+            TextView tv = new TextView(this);
+            tv.setText(String.valueOf(valor));
+            ll.addView(tv);
+        }
     }
 }
